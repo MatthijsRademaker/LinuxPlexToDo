0d0dafbf5d2c34fa34e552ae25e7cb70b45d1968
Matthijs Rademaker <matthijsrademaker33@gmail.com>
2024-11-22 19:20:31 +0100

    feat: added immich and refactoring

--- Launchscripts/launch.sh
+++ Launchscripts/launch.sh
@@ -5,6 +5,9 @@ echo "Starting all services..."
 cd ~/Repos/LinuxPlexToDo/Launchscripts/monitoring
 docker compose up -d
 
+cd ~/Repos/LinuxPlexToDo/Launchscripts/immich
+docker compose up -d
+
 cd ~/Repos/LinuxPlexToDo/Launchscripts/plex
 docker compose up -d
 
